0a78d3988b8c709ba75f0c975ca120d6e8f9770f
Grayson Gilmore <gilmoreg@live.com>
2017-03-13 13:42:52 -0500

    Add Form component

---
+++ client/src/components/MainView/TicketView/Form/Form.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Form from './Form';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Form />, div);
+});
