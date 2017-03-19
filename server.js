e171782b635881ea9ca1711ee8edecf15fa3e0cf
Grayson Gilmore <gilmoreg@live.com>
2017-03-18 22:14:26 -0500

    Packages

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const { router } = require('./routes');
+const { logger } = require('./logger');
+
+require('dotenv').config();
+
+const app = express();
+let server;
+
+app.use(cors());
+app.use(router);
+app.use(morgan('common', { stream: logger.stream }));
+
+function runServer(port = process.env.PORT) {
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      logger.log(`Your app is listening on port ${port}`);
+      resolve();
+    })
+    .on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    logger.log('Closing server');
+    server.close((err) => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => logger.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
