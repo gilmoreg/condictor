3baf0a156702d0a3533646d7460cee6e59412fbc
Grayson Gilmore <gilmoreg@live.com>
2017-03-31 23:29:04 -0500

    Fix store export

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
-import { Provider } from 'react-redux';
 import { store, client } from './store';
 import Condictor from './components/Condictor';
 import './index.css';
 
-console.log(store);
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Provider store={store}>
-      <Condictor />
-    </Provider>
+  <ApolloProvider store={store} client={client}>
+    <Condictor />
   </ApolloProvider>,
   document.getElementById('root'),
 );
