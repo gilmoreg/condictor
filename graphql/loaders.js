5e8b0a83ddb3780026d7ce69743c7b34980711f9
Grayson Gilmore <gilmoreg@live.com>
2017-04-01 22:20:56 -0500

    Add loaders for find-alls

--- graphql/loaders.js
+++ graphql/loaders.js
@@ -13,22 +13,42 @@ const ConsumerLoader = new DataLoader(
   keys => Promise.all(keys.map(key => Consumer.findById(key))),
 );
 
+const ConsumersLoader = new DataLoader(
+  keys => Promise.all(keys.map(() => Consumer.find())),
+);
+
 const ProductLoader = new DataLoader(
   keys => Promise.all(keys.map(key => Product.findById(key))),
+);
+
+const ProductsLoader = new DataLoader(
+  keys => Promise.all(keys.map(() => Product.find())),
 );
 
 const TicketLoader = new DataLoader(
   keys => Promise.all(keys.map(key => Ticket.findById(key))),
 );
 
+const TicketsLoader = new DataLoader(
+  keys => Promise.all(keys.map(() => Ticket.find())),
+);
+
 const UserLoader = new DataLoader(
   keys => Promise.all(keys.map(key => User.findById(key))),
+);
+
+const UsersLoader = new DataLoader(
+  keys => Promise.all(keys.map(() => User.find())),
 );
 
 module.exports = {
   CommentLoader,
   ConsumerLoader,
+  ConsumersLoader,
   ProductLoader,
+  ProductsLoader,
   TicketLoader,
+  TicketsLoader,
   UserLoader,
+  UsersLoader,
 };
