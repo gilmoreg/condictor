08c54192b6057b686a9c0a1bd7f03eadb1cd7bd6
Grayson Gilmore <gilmoreg@live.com>
2017-04-15 19:04:44 -0500

    Finish NewComment

--- graphql/types/NewComment.js
+++ graphql/types/NewComment.js
@@ -1,10 +1,28 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-underscore-dangle */
-// import Comment from '../../models/Comment';
-// import { UserLoader, TicketLoader } from '../loaders';
+import Comment from '../../models/Comment';
+import Ticket from '../../models/Ticket';
 
 export default class NewCommentHandler {
   constructor(ticketID, input) {
-    console.log('New comment', ticketID, input);
+    if (ticketID && input.owner && input.description) {
+      return Comment.create({
+        owner: input.owner,
+        description: input.description,
+        created: Date.now(),
+      })
+      .then((comment) => {
+        if (comment) {
+          return Ticket.findByIdAndUpdate(
+            ticketID,
+            { $push: { comments: comment._id } },
+          )
+          .catch(err => new Error(err));
+        }
+        return new Error('Comment not created.');
+      })
+      .catch(err => new Error(err));
+    }
+    return null;
   }
 }
