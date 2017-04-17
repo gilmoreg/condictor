f5a0fe1019b1bef9f65723dc97ffeb580b2bebe4
Grayson Gilmore <gilmoreg@live.com>
2017-04-16 19:07:44 -0500

    NewComment updates by user id

--- client/src/components/MainView/TicketView/TicketList/TicketListItem/Ticket/Comment/AddComment.js
+++ client/src/components/MainView/TicketView/TicketList/TicketListItem/Ticket/Comment/AddComment.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { addComment } from '../../../../../../../actions';
 import './AddComment.css';
 
 export class AddComment extends Component {
   constructor(props) {
     super(props);
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({ text: e.target.value });
+  }
+
+  submit() {
+    this.props.dispatch(addComment(this.props.ticketID, this.state.text));
   }
 
   render() {
     return (
       <div className="AddComment">
-        Add a comment! {this.props.ticketID}
+        <textarea onChange={this.handleChange} />
+        <button onClick={this.submit}>Add Comment</button>
       </div>
     );
   }
 }
 
 AddComment.defaultProps = {
+  ticketID: '',
   dispatch: () => {},
 };
 
 AddComment.propTypes = {
+  ticketID: PropTypes.string,
   dispatch: PropTypes.func.isRequired,
 };
 
--- graphql/types/NewComment.js
+++ graphql/types/NewComment.js
@@ -2,26 +2,29 @@
 /* eslint-disable no-underscore-dangle */
 import Comment from '../../models/Comment';
 import Ticket from '../../models/Ticket';
+import User from '../../models/User';
 
 export default class NewCommentHandler {
   constructor(ticketID, input) {
     if (ticketID && input.owner && input.description) {
-      return Comment.create({
-        owner: input.owner,
-        description: input.description,
-        created: Date.now(),
-      })
-      .then((comment) => {
-        if (comment) {
-          return Ticket.findByIdAndUpdate(
-            ticketID,
-            { $push: { comments: comment._id } },
-          )
-          .catch(err => new Error(err));
-        }
-        return new Error('Comment not created.');
-      })
-      .catch(err => new Error(err));
+      return User.findOne({ username: input.owner })
+        .then((user) => {
+          if (!user) throw new Error('User not found.');
+          Comment.create({
+            owner: user._id,
+            description: input.description,
+            created: Date.now(),
+          })
+          .then((comment) => {
+            if (!comment) throw new Error('Comment not found.');
+            console.log('adding comment to ticket', ticketID, comment);
+            return Ticket.findByIdAndUpdate(
+              ticketID,
+              { $push: { comments: comment._id } },
+            );
+          });
+        })
+        .catch(err => new Error(err));
     }
     return null;
   }
