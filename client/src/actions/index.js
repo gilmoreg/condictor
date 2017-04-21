5bdcf9c1758c7bb7e7f72d5c92a9b09a274c58b2
Grayson Gilmore <gilmoreg@live.com>
2017-04-20 20:23:20 -0500

    Alphabetize actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,30 +8,6 @@ const client = new Lokka({
 });
 
 // Sync Actions
-export const CLEAR_TICKETS = 'CLEAR_TICKETS';
-export const clearTickets = () => ({
-  type: CLEAR_TICKETS,
-});
-
-export const UPDATE_TICKET = 'UPDATE_TICKET';
-export const updateTicket = ticket => ({
-  type: UPDATE_TICKET,
-  ticket,
-});
-
-export const MARK_TICKET_CLOSED = 'MARK_TICKET_CLOSED';
-export const markTicketClosed = (id, closed) => ({
-  type: MARK_TICKET_CLOSED,
-  id,
-  closed,
-});
-
-export const FILL_TICKET = 'FILL_TICKET';
-export const fillTicket = ticket => ({
-  type: FILL_TICKET,
-  ticket,
-});
-
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const addComment = (ticketID, comment) => ({
   type: ADD_COMMENT,
@@ -48,6 +24,11 @@ export const addTicket = ticket => ({
 export const CLEAR_SEARCH_OPTIONS = 'CLEAR_SEARCH_OPTIONS';
 export const clearSearchOptions = () => ({
   type: CLEAR_SEARCH_OPTIONS,
+});
+
+export const CLEAR_TICKETS = 'CLEAR_TICKETS';
+export const clearTickets = () => ({
+  type: CLEAR_TICKETS,
 });
 
 export const FILL_CONSUMER_OPTION = 'FILL_CONSUMER_OPTION';
@@ -68,150 +49,46 @@ export const fillProductOption = product => ({
   product,
 });
 
+export const FILL_TICKET = 'FILL_TICKET';
+export const fillTicket = ticket => ({
+  type: FILL_TICKET,
+  ticket,
+});
+
 export const FILL_USER = 'FILL_USER';
 export const fillUser = user => ({
   type: FILL_USER,
   user,
 });
 
+export const MARK_TICKET_CLOSED = 'MARK_TICKET_CLOSED';
+export const markTicketClosed = (id, closed) => ({
+  type: MARK_TICKET_CLOSED,
+  id,
+  closed,
+});
+
+export const UPDATE_TICKET = 'UPDATE_TICKET';
+export const updateTicket = ticket => ({
+  type: UPDATE_TICKET,
+  ticket,
+});
+
 // Async Actions
-export const LOGIN = 'LOGIN';
-export const login = credentials => dispatch =>
-  fetch(`${API_URL}/login`, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      Accept: 'application/json, */*',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      username: credentials.username,
-      password: credentials.password,
-    }),
-  })
-  .then((res) => {
-    if (res.status !== 200) throw new Error('Login failed');
-    return res.json();
-  })
-  .then((res) => {
-    dispatch(fillUser(res.user));
-  })
-  .catch(() => {
-    dispatch(fillUser({ error: 'Login failed' }));
-  });
-
-export const LOGOUT = 'LOGOUT';
-export const logout = () => dispatch =>
-  fetch(`${API_URL}/logout`, {
-    credentials: 'include',
-  })
-  .then(() => {
-    dispatch(fillUser(null));
-  });
-
-export const SESSION_CHECK = 'SESSION_CHECK';
-export const sessionCheck = () => dispatch =>
-  fetch(`${API_URL}/check`, {
-    credentials: 'include',
-  })
-  .then(res => res.json())
-  .then((res) => {
-    if (res.user) dispatch(fillUser(res.user));
-    else dispatch(fillUser(null));
-  });
-
-export const SEARCH_TICKETS = 'SEARCH_TICKETS';
-export const searchTickets = params => dispatch =>
-  new Promise((resolve, reject) => {
-    let options = '';
-    if (Object.keys(params).length) {
-      options = '(';
-      Object.keys(params).forEach((key) => {
-        // 'open' is a boolean, cannot have quote marks
-        console.log('search ticket key', key);
-        if (key === 'open') options += `${key}: ${params[key]} `;
-        else options += `${key}: "${params[key]}" `;
-      });
-      options += ')';
-    }
-    const query = `
-      query {
-        search${options} {
-          results {
-            id
-            priority
-            created
-            closed
-            owner {
-              username
-            }
-            description
-            consumer {
-              name
-            }
-            product {
-              name
-            }
-            comments {
-              id
-              created
-              owner {
-                username
-              }
-              description
-            }
-          }
-        }
-      }
-    `;
-    client.query(query)
-    .then((response) => {
-      if (response.search && response.search.results) {
-        dispatch(clearTickets());
-        response.search.results.forEach(ticket => dispatch(fillTicket(ticket)));
-        resolve(response.search.results);
-      } else {
-        reject('no results');
-      }
-    })
-    .catch(err => reject(err));
-  });
-
-export const FILL_SEARCH_OPTIONS = 'FILL_SEARCH_OPTIONS';
-export const fillSearchOptions = () => dispatch =>
+export const CLOSE_TICKET = 'CLOSE_TICKET';
+export const closeTicket = id => dispatch =>
   new Promise((resolve, reject) => {
     client.query(`
-      query {
-        products {
-          products {
-            id
-            name
-          }
-        }
-        consumers {
-          consumers {
-            id
-            name
-          }
-        }
-        users {
-          users {
-            id
-            username
-          }
+      mutation {
+        closeTicket(id: "${id}") {
+          id
+          closed
         }
       }
     `)
-    .then((options) => {
-      if (!options) throw new Error('Server response empty');
-      dispatch(clearSearchOptions());
-      const { consumers } = options.consumers;
-      const { products } = options.products;
-      const { users } = options.users;
-      if (consumers) consumers.forEach(consumer => dispatch(fillConsumerOption(consumer)));
-      if (products) products.forEach(product => dispatch(fillProductOption(product)));
-      if (users) users.forEach(user => dispatch(fillOwnerOption(user)));
-      resolve(options);
+    .then((ticket) => {
+      dispatch(markTicketClosed(ticket.id, ticket.closed));
+      resolve(ticket);
     })
     .catch(err => reject(err));
   });
@@ -287,20 +164,143 @@ export const createTicket = fields => dispatch =>
     .catch(err => reject(err));
   });
 
-export const CLOSE_TICKET = 'CLOSE_TICKET';
-export const closeTicket = id => dispatch =>
+export const FILL_SEARCH_OPTIONS = 'FILL_SEARCH_OPTIONS';
+export const fillSearchOptions = () => dispatch =>
   new Promise((resolve, reject) => {
     client.query(`
-      mutation {
-        closeTicket(id: "${id}") {
-          id
-          closed
+      query {
+        products {
+          products {
+            id
+            name
+          }
+        }
+        consumers {
+          consumers {
+            id
+            name
+          }
+        }
+        users {
+          users {
+            id
+            username
+          }
         }
       }
     `)
-    .then((ticket) => {
-      dispatch(markTicketClosed(ticket.id, ticket.closed));
-      resolve(ticket);
+    .then((options) => {
+      if (!options) throw new Error('Server response empty');
+      dispatch(clearSearchOptions());
+      const { consumers } = options.consumers;
+      const { products } = options.products;
+      const { users } = options.users;
+      if (consumers) consumers.forEach(consumer => dispatch(fillConsumerOption(consumer)));
+      if (products) products.forEach(product => dispatch(fillProductOption(product)));
+      if (users) users.forEach(user => dispatch(fillOwnerOption(user)));
+      resolve(options);
     })
     .catch(err => reject(err));
+  });
+
+export const LOGIN = 'LOGIN';
+export const login = credentials => dispatch =>
+  fetch(`${API_URL}/login`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json, */*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: credentials.username,
+      password: credentials.password,
+    }),
+  })
+  .then((res) => {
+    if (res.status !== 200) throw new Error('Login failed');
+    return res.json();
+  })
+  .then((res) => {
+    dispatch(fillUser(res.user));
+  })
+  .catch(() => {
+    dispatch(fillUser({ error: 'Login failed' }));
+  });
+
+export const LOGOUT = 'LOGOUT';
+export const logout = () => dispatch =>
+  fetch(`${API_URL}/logout`, {
+    credentials: 'include',
+  })
+  .then(() => {
+    dispatch(fillUser(null));
+  });
+
+export const SEARCH_TICKETS = 'SEARCH_TICKETS';
+export const searchTickets = params => dispatch =>
+  new Promise((resolve, reject) => {
+    let options = '';
+    if (Object.keys(params).length) {
+      options = '(';
+      Object.keys(params).forEach((key) => {
+        // 'open' is a boolean, cannot have quote marks
+        console.log('search ticket key', key);
+        if (key === 'open') options += `${key}: ${params[key]} `;
+        else options += `${key}: "${params[key]}" `;
+      });
+      options += ')';
+    }
+    const query = `
+      query {
+        search${options} {
+          results {
+            id
+            priority
+            created
+            closed
+            owner {
+              username
+            }
+            description
+            consumer {
+              name
+            }
+            product {
+              name
+            }
+            comments {
+              id
+              created
+              owner {
+                username
+              }
+              description
+            }
+          }
+        }
+      }
+    `;
+    client.query(query)
+    .then((response) => {
+      if (response.search && response.search.results) {
+        dispatch(clearTickets());
+        response.search.results.forEach(ticket => dispatch(fillTicket(ticket)));
+        resolve(response.search.results);
+      } else {
+        reject('no results');
+      }
+    })
+    .catch(err => reject(err));
+  });
+
+export const SESSION_CHECK = 'SESSION_CHECK';
+export const sessionCheck = () => dispatch =>
+  fetch(`${API_URL}/check`, {
+    credentials: 'include',
+  })
+  .then(res => res.json())
+  .then((res) => {
+    if (res.user) dispatch(fillUser(res.user));
+    else dispatch(fillUser(null));
   });
