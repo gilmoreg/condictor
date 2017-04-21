1168ae55215144702e4ee070ef1f231589f13276
Grayson Gilmore <gilmoreg@live.com>
2017-04-20 21:13:25 -0500

    Alphabetize reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -11,21 +11,6 @@ const initialState = {
 export default function rootReducer(state = initialState, action) {
   console.log(action.type, 'dispatched');
   switch (action.type) {
-    case actions.CLEAR_TICKETS: {
-      return Object.assign({}, state, { tickets: [] });
-    }
-    case actions.FILL_TICKET: {
-      return Object.assign({}, state, { tickets: [...state.tickets, action.ticket] });
-    }
-    case actions.MARK_TICKET_CLOSED: {
-      const tickets = state.tickets.map((ticket) => {
-        if (action.id !== ticket.id) {
-          return ticket;
-        }
-        return Object.assign({}, ticket, { closed: action.closed });
-      });
-      return Object.assign({}, state, { tickets });
-    }
     case actions.ADD_COMMENT: {
       const tickets = state.tickets.map((ticket) => {
         if (`${action.ticketID}` !== ticket.id) {
@@ -35,21 +20,45 @@ export default function rootReducer(state = initialState, action) {
       });
       return Object.assign({}, state, { tickets });
     }
+
     case actions.CLEAR_SEARCH_OPTIONS: {
       return Object.assign({}, state, { consumers: [], owners: [], products: [] });
     }
+
+    case actions.CLEAR_TICKETS: {
+      return Object.assign({}, state, { tickets: [] });
+    }
+
     case actions.FILL_CONSUMER_OPTION: {
       return Object.assign({}, state, { consumers: [...state.consumers, action.consumer] });
     }
+
     case actions.FILL_OWNER_OPTION: {
       return Object.assign({}, state, { owners: [...state.owners, action.owner] });
     }
+
     case actions.FILL_PRODUCT_OPTION: {
       return Object.assign({}, state, { products: [...state.products, action.product] });
     }
+
+    case actions.FILL_TICKET: {
+      return Object.assign({}, state, { tickets: [...state.tickets, action.ticket] });
+    }
+
     case actions.FILL_USER: {
       return Object.assign({}, state, { user: action.user });
     }
+
+    case actions.MARK_TICKET_CLOSED: {
+      const tickets = state.tickets.map((ticket) => {
+        if (action.id !== ticket.id) {
+          return ticket;
+        }
+        return Object.assign({}, ticket, { closed: action.closed });
+      });
+      return Object.assign({}, state, { tickets });
+    }
+
     default: return state;
   }
 }
