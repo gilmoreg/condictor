82c32376b47de7a2c415fece9ffb06ce57d66cba
Grayson Gilmore <gilmoreg@live.com>
2017-06-13 12:08:02 -0500

    Add HowTo text and styling

--- src/components/TicketView/Form/NewTicket/NewTicketHowto.js
+++ src/components/TicketView/Form/NewTicket/NewTicketHowto.js
@@ -3,7 +3,17 @@ import './NewTicketHowto.css';
 
 const NewTicketHowto = () =>
   <div className="new-ticket-howto">
-    <p>(Explain how this works)</p>
+    <h3>Creating a new ticket</h3>
+    <p>A consumer of your company's product has reported a problem with one of your company's products. Document their issue by creating a new ticket!</p>
+    <p>A <strong>Condictor</strong> ticket has the following properties:</p>
+    <ul>
+      <li>A <em>priority</em> from 1 (highest) to 5 (lowest).</li>
+      <li>A <em>consumer</em> which you can select from a pre-established list in the database.</li>
+      <li>The <em>product</em> which the consumer is having trouble with.</li>
+      <li>A <em>description</em> of the problem.</li>
+    </ul>
+    <p><strong>The first line of the description will identify this ticket in search results, so make it brief and descriptive</strong>, and follow it up with as much detail as necessary. If you are familiar with <strong>Git</strong> commit messages, the idea is similar.</p>
+    <p>Create the ticket by clicking the <strong>Create Ticket</strong> button, and your new ticket will appear below. From there you or your colleagues can add comments and close the ticket if necessary.</p>
   </div>;
 
 export default NewTicketHowto;
--- src/components/TicketView/Form/Search/SearchHowto.js
+++ src/components/TicketView/Form/Search/SearchHowto.js
@@ -3,7 +3,11 @@ import './SearchHowto.css';
 
 const SearchHowto = () =>
   <div className="search-howto">
-    <p>(Explain how this works)</p>
+    <h3>Searching for existing tickets</h3>
+    <p><strong>Condictor</strong> allows you to search existing tickets by <em>consumer</em>, <em>product</em>, or <em>owner</em> (the person who created the ticket).</p>
+    <p>Leaving any of the fields blank will ignore them in the search; <strong>leaving all of the fields blank will return all tickets</strong>.</p>
+    <p>The <em>Only Open</em> option allows you to filter out closed tickets.</p>
+    <p><strong>Try searching with all fields blank first</strong>. The more specific your search is, the less likely it will return anything.</p>
   </div>;
 
 export default SearchHowto;
